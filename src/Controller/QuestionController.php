ad8b7c8dc2e4253959886191ff2425fc97a8e167
Sigri44 <sigri44@hotmail.fr>
2019-01-29 10:22:38 +0100

    Update Question Controller/Entity

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -13,22 +13,16 @@ class QuestionController extends AbstractController
      */
     public function list()
     {
+        //ce repository nous permet de faire des SELECT
         $questionRepository = $this->getDoctrine()->getRepository(Question::class);
+
+        //SELECT * FROM question WHERE status = 'debating' ORDER BY supports DESC LIMIT 1000
         $questions = $questionRepository->findBy(
             ['status' => 'debating'], //where
             ['supports' => 'DESC'], //order by
             1000, //limit
             0 //offset
         );
-
-        //dd($questions);
-
-        /*
-        foreach ($questions as $question)
-        {
-            dump($question);
-        }
-        */
 
         return $this->render('question/list.html.twig', array(
             'questions' => $questions,
